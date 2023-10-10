8861c31417d815a394c0a2c5fbbbfb222c9e871c
mhchia <kevin.mh.chia@gmail.com>
2023-10-10 15:56:52 +0800

    feat: pass arguments from js

--- worker.js
+++ worker.js
@@ -27,11 +27,40 @@ class Test {
         // 6422528 ~= 6.12 mb
         console.log("!@# res.memory.buffer.length=", res.memory.buffer.byteLength)
         await initThreadPool(numConcurrency);
-        const resProver = await prover();
-        console.log("!@# resProver=", resProver)
+
+        const maxTranscriptSize = 1 << 14;
+        const notaryHost = "localhost";
+        const notaryPort = 7047;
+
+        const serverDomain = "twitter";
+        const route = "i/api/1.1/dm/conversation";
+        const conversationId = "";
+        const clientUuid = "";
+        const authToken = "";
+        const accessToken = "";
+        const csrfToken = "";
+        const userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36";
+        const websocketProxyURL = "ws://localhost:55688";
+
+        const resProver = await prover(
+            maxTranscriptSize,
+            notaryHost,
+            notaryPort,
+            serverDomain,
+            route,
+            conversationId,
+            clientUuid,
+            userAgent,
+            authToken,
+            accessToken,
+            csrfToken,
+            websocketProxyURL,
+        );
+        const resJSON = JSON.parse(resProver);
         console.log("!@# resAfter.memory=", res.memory)
         // 1105920000 ~= 1.03 gb
         console.log("!@# resAfter.memory.buffer.length=", res.memory.buffer.byteLength)
+        console.log("!@# resJSON=", resJSON)
     }
 }
 
